9d5e22f2b9e90437c0010a4e936c0515f81d5ac8
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2025-03-18 22:43:03 +0530

    user authanication , getuser , getalluser , login logout created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,25 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile
+} from "../controllers/userController.js";
+
+import { authanticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').post(createUser);
+router
+  .route("/")
+  .post(createUser)
+  .get(authanticate, authorizeAdmin, getAllUsers);
+
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+router.route("/profile").get(authanticate, getCurrentUserProfile).put(authanticate , updateCurrentUserProfile);
 
 export default router;
